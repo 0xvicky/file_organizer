0c61dcb9ab13c244eba8d7647eace557cf272f2b
vicky <tyagivivek528@gmail.com>
2025-02-23 17:26:35 +0530

    all done

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ fn main() {
             match entry {
                 Ok(entry) => {
                     let path = entry.path();
+                    let file_name = path.file_name().unwrap();
                     //-> if it is dir, then skip it
                     if path.is_dir() {
                         continue; // skip the sub dirs
@@ -35,6 +36,15 @@ fn main() {
                                     }
                                 }
                                 //move the current entry to the created directory or existed directory
+                                //source path -> DUMMY_DIR+FILENAME
+                                let source_path = Path::new(DUMMY_PATH).join(file_name);
+                                //dest path -> DUMMY_DIR + ext_str + FILENAME
+                                let dest_path =
+                                    Path::new(DUMMY_PATH).join(&ext_str).join(file_name);
+                                match fs::rename(source_path, dest_path) {
+                                    Ok(_) => (),
+                                    Err(e) => println!("Error occured while moving the file{}", e),
+                                }
                             }
                             None => println!("Error extracting extension"),
                         }
